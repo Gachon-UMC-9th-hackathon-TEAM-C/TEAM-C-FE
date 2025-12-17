498336d6be066c4d9892773d5825ef55e543521e
smileman62 <jskim6335@naver.com>
2025-12-17 23:42:09 +0900

    :sparkles: 공용 버튼 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
+import Button from "../component/common/Button";
 import HomeHeader from "../component/Home/HomeHeader";
 
 
 const Home = () => {
     return(
     <div className="flex flex-1 w-full">
-        <HomeHeader />    
+        <HomeHeader />
+        <Button children="test" variant="blue"/> 
+        <Button children="test" variant="white" />
     </div>);
 }
 
